99dfa74f8edcdc6460e2e2fe0b051b0d94aac9c9
Richa Virmani <42806102+rvirmani6@users.noreply.github.com>
2020-03-19 17:09:30 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'TpUWUIww9ntdD1VRj87dI37ZW',
+  consumer_secret:      't4GnizWPZl475VngKWGGbtsxl33t0Fbuxt5bAkV5FePWXNyE8d',
+  access_token:         '1056963843883175936-QDuKpk4akQCqlOZlDSZ3pEqb3SbGK1',
+  access_token_secret:  'HxJ05N3Lc6Fx8LBWSyjCCHo7Xc1Ln8UpkOitZ8vgsipHn'
+}
